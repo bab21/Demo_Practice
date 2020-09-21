b1547521dda78163b3f2844f209246f11113584c
bab21 <38082057+bab21@users.noreply.github.com>
2020-09-21 12:09:38 +0530

    Update App.java

--- src/main/java/NewStart/JavaPractice/App.java
+++ src/main/java/NewStart/JavaPractice/App.java
@@ -11,7 +11,7 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Changes for branch test" );
+        System.out.println( "Hello World! Changes for conflict " );
         logger.trace("We've just greeted the user!");
         logger.debug("We've just greeted the user!");
         logger.info("We've just greeted the user!");
