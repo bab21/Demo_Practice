6296f8fd8269367f7038fb51de4b37a5fc95a000
bab21 <2016061@iiitdmj.ac.in>
2020-09-21 11:23:10 +0530

    Changes added

--- src/main/java/NewStart/JavaPractice/App.java
+++ src/main/java/NewStart/JavaPractice/App.java
@@ -11,7 +11,7 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! There" );
+        System.out.println( "Hello World! Changes for branch test" );
         logger.trace("We've just greeted the user!");
         logger.debug("We've just greeted the user!");
         logger.info("We've just greeted the user!");
